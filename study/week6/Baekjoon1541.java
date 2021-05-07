af22f16428566f879f5868a8b09ded490fe7ba40
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2021-05-07 16:50:08 +0900

    리드미 업데이트 (#7)

---
+++ study/week6/Baekjoon1541.java
@@ -0,0 +1,16 @@
+package study.week6;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        StringBuilder sb = new StringBuilder(br.readLine());
+        
+    }
+
+}
