da8367296ec3c1eb64d3d70535d0a7f2db7cbbf8
jeongjun Hwang <hsk2454@naver.com>
2021-05-19 17:51:18 +0900

    7주차 문제풀이 업로드

--- study/week7/Baekjoon1766.java
+++ study/week7/Baekjoon1766.java
@@ -1,7 +1,19 @@
+package study.week7;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 문제집
+ * 링크 : https://www.acmicpc.net/problem/1766
+ * 알고리즘 분류
+ * - 자료구조
+ * - 그래프이론
+ * - 우선순위 큐
+ * - 위상 정렬
+ */
 
 public class Baekjoon1766 {
     public static void main(String[] args) throws IOException {
@@ -17,22 +29,20 @@ public class Baekjoon1766 {
         ArrayList<Integer>[] nextProblem = new ArrayList[n+1];
         for (int i = 0; i < n+1; i++) 
             nextProblem[i] = new ArrayList<>();
-        int[] prevProblem = new int[n+1];
-        boolean[] isSolved = new boolean[n+1];
-        
+        int[] prevProblemCount = new int[n+1];
+    
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             nextProblem[a].add(b);
-            prevProblem[b] = a;
+            prevProblemCount[b]++;
         }
 
         for (int i = 1; i < n+1; i++) { 
         //이전에 풀어야 하는 문제가 없는 문제를 우선순위 큐에 삽입
-            if(prevProblem[i] == 0) {
+            if(prevProblemCount[i] == 0) {
                 pq.add(i);
-                isSolved[i] = true;
             }
         }
         
@@ -40,10 +50,9 @@ public class Baekjoon1766 {
             int currItem = pq.poll();
             for (int i = 0; i < nextProblem[currItem].size(); i++) {
                 int nextItem = nextProblem[currItem].get(i);
-                if(nextItem != 0 && !isSolved[nextItem])  {
+                prevProblemCount[nextItem]--;
+                if(prevProblemCount[nextItem] == 0)  
                     pq.add(nextItem);
-                    isSolved[nextItem] = true;
-                }
             }
             bw.write(currItem+" ");
         }
@@ -51,57 +60,3 @@ public class Baekjoon1766 {
         bw.flush();
     }
 }
-/*
-8 3
-5 2
-7 4
-4 3
-
-1 5 2 6 7 4 3 8
-
-5 3
-3 1
-3 2
-5 3
-
-next
-0 1 2 3 4 5
-0 0 0 1 0 3
-      2
-
-prev
-0 1 2 3 4 5
-0 3 3 5 0 0
-
-4 5
-4
-
-5
-4 5
-
-3
-4 5 3
-
-1 2
-4 5 3 1
-
-2
-4 5 3 1 2
-
-
-5 2
-3 1
-4 1
-
-next
-0 1 2 3 4 5
-0 0 0 1 0 3
-      2
-
-prev
-0 1 2 3 4 5
-0 3 3 5 0 0
-
-v
-
-*/
