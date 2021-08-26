1ac7c7776a5defcfad2b740482f97d5439044a5e
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2021-08-26 09:10:27 +0900

    문제풀이 업로드 (#34)

--- study/Baekjoon/month8_4/Baekjoon1753_2.java
+++ study/Baekjoon/month8_4/Baekjoon1753_2.java
@@ -1,35 +1,21 @@
 package study.Baekjoon.month8_4;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * 문제이름 : 최단경로
  * 링크 : https://www.acmicpc.net/problem/1753
+ * 통과는 못하지만 2차원 배열로 간단하게 구현한 코드
  */
 
-public class Baekjoon1753 {
-    static class Edge implements Comparable<Edge>{
-        public int nextVertex;
-        public int weight;
-        public Edge(int toVertex, int weight) {
-            this.nextVertex = toVertex;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
-        }
-        
-        
-    }
+public class Baekjoon1753_2 {
 
     static int V, E, K;
     static int[] distance;
     static boolean[] visited;
-    static ArrayList<Edge>[] graph;
+    static int[][] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -42,10 +28,10 @@ public class Baekjoon1753 {
 
         distance = new int[V+1];
         visited = new boolean[V+1];
-        graph = new ArrayList[V+1];
+        graph = new int[V+1][V+1];
         for (int i = 0; i < V+1; i++) {
             distance[i] = Integer.MAX_VALUE;
-            graph[i] = new ArrayList<>();
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
         }
 
         for (int i = 0; i < E; i++) {
@@ -53,7 +39,7 @@ public class Baekjoon1753 {
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Edge(v, w));
+            graph[u][v] = w;
         }
 
         dijkstra();
@@ -68,21 +54,27 @@ public class Baekjoon1753 {
     }
 
     public static void dijkstra() {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(K, 0)); //시작지점 삽입
-        distance[K] = 0;    //시작지점 가중치 0
-        while (!pq.isEmpty()) {
-            Edge edge = pq.poll();
-            int curr = edge.nextVertex;
-            if (visited[curr]) 
-                continue;
+        distance[K] = 0;    //시작지점 거리 입력
+
+        int min, curr;
+        for (int i = 1; i < V+1; i++) {
+            min = Integer.MAX_VALUE;
+            curr = -1;
+            for (int j = 1; j < V+1; j++) {
+                if (!visited[j] && distance[j] < min) {
+                    min = distance[j];
+                    curr = j;
+                }
+            }
+            if (curr == -1)    //더이상 정점이 없으면 탈출
+                break;
+            visited[curr] = true; // 선택정점 방문 처리
             
-            visited[curr] = true;
-            for (Edge next : graph[curr]) {
-                int nextWeight = distance[curr] + next.weight;
-                if (distance[next.nextVertex] >= nextWeight) {
-                    distance[next.nextVertex] = nextWeight;
-                    pq.add(new Edge(next.nextVertex, nextWeight));
+            for (int c = 1; c < V+1; c++) {
+                int nextWeight = min + graph[curr][c];
+                if (!visited[c] && graph[curr][c] != Integer.MAX_VALUE && nextWeight < distance[c]) {
+                    // 방문하지 않음 && 간선이 있음 && 계산한 새로 도출된 거리가 distance배열 안의 값보다 작음 
+                    distance[c] = nextWeight;
                 }
             }
         }
