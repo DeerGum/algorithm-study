1a7ceb916b007ff642bf0060df7b7acac0ca83e7
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2021-10-27 21:43:15 +0900

    문제풀이 및 리드미업데이트 (#50)

---
+++ study/newStudy/Baekjoon0000.java
@@ -0,0 +1,23 @@
+package study.newStudy;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 
+ * 링크 : https://www.acmicpc.net/problem/
+ */
+
+public class Baekjoon0000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+    }
+}
+
+/*
+
+
+*/
---
+++ study/old/week11/Baekjoon1052.java
@@ -0,0 +1,34 @@
+package study.old.week11;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 물병
+ * 링크 : https://www.acmicpc.net/problem/1052
+ * 알고리즘 분류
+ * - 그리디알고리즘
+ * - 비트마스킹
+ * - 구현
+ */
+
+public class Baekjoon1052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int newBottles = 0;
+
+		  while(Integer.bitCount(n++) > k) {
+			  newBottles++;
+		  }
+        
+        bw.write(newBottles+"\n");
+        bw.flush();
+    }
+}
