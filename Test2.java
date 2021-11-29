92966f46670b57fb780fb05e1445e3ce3d85b3b6
황정준 <hsk2454@naver.com>
2021-11-29 23:14:47 +0900

    문제 풀이중

---
+++ Test2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+
+public class Test2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+
+		String result = formatting(num);
+		System.out.println(result);
+	}
+
+	public static String formatting(int num) {
+		
+		StringBuilder format = new StringBuilder(String.valueOf(num));
+		int mod = format.length() % 3;
+
+		for (int i = 1; i < format.length(); i++) {
+			if (i % 3 == mod) {
+				format.insert(i, ',');
+				
+			}
+		}
+
+		return format.toString();
+	}
+}
+
+/*
+1,000 size = 4 mod3 = 1
+0 123
+
+10,000 size = 5 mod3 = 2
+01 234
+
+100,000 size = 6 mod3 = 0
+012 345
+
+1,000,000 size = 7 mod3 = 1
+0 123 456
+
+*/
