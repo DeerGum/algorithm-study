98c09fea3dfe23c9248adb96b114b05fa9002547
jeongjun Hwang <hsk2454@naver.com>
2021-12-06 10:17:57 +0900

    레이저 통신 문제풀이

--- study/newStudy/week3/Baekjoon6087.java
+++ study/newStudy/week3/Baekjoon6087.java
@@ -1,8 +1,7 @@
-// package study.newStudy.week3;
+package study.newStudy.week3;
 
 import java.io.*;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
@@ -15,6 +14,7 @@ public class Baekjoon6087 {
     static int W, H;
     static char[][] map;
     static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    static int[][] v;
     static int result;
     static int endR;
     static int endC;
@@ -50,21 +50,17 @@ public class Baekjoon6087 {
             }
         }
 
-        result = 0;
+        result = Integer.MAX_VALUE;
         bfs(startR, startC);
-        for (int i = 0; i < H; i++) {
-            bw.write(Arrays.toString(map[i]) + "\n");
-        }
         bw.write(result + "\n");
         bw.close();
-
     }
 
     public static void bfs(int r, int c) {
         Deque<int[]> dq = new ArrayDeque<>();
 
         dq.addFirst(new int[] { r, c, -1, -1 });
-        int[][] v = new int[H][W];
+        v = new int[H][W];
         v[r][c] = 1;
 
         while (!dq.isEmpty()) {
@@ -74,8 +70,6 @@ public class Baekjoon6087 {
             int prevDir = curr[2];
             int currCost = curr[3];
 
-            // printArray();
-            // System.out.println();
             if (currR == endR && currC == endC) {
                 result = Math.min(result, currCost);
                 continue;
@@ -97,26 +91,8 @@ public class Baekjoon6087 {
                     v[dr][dc] = nextCost;
                     dq.addLast(new int[]{dr, dc, i, nextCost});
                 }
-                // if (i == prevDir) { // 이전 방향이랑 같으면
-                //     if (map[dr][dc] > currCost) {
-                //         dq.addFirst(new int[] { dr, dc, i, currCost });
-                //         map[dr][dc] = currCost;
-                //     }
-                // } else if (prevDir != rDirIdx[i]) { // 왔던 방향이 아니고 이전 방향이랑 다르면
-                //     if (map[dr][dc] > nextCost) {
-                //         dq.addLast(new int[] { dr, dc, i, nextCost });
-                //         map[dr][dc] = nextCost;
-                //     }
-                // }
                 
             }
-        }
-
-    }
-
-    public static void printArray() {
-        for (int i = 0; i < map.length; i++) {
-            System.out.println(Arrays.toString(map[i]));
         }
     }
 }
