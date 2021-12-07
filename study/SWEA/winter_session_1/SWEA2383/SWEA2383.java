7ee3780102d2d4d708064bb7d64996ed02b7a0f5
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2021-12-07 13:18:40 +0900

    문제풀이 업로드 (#58)

---
+++ study/SWEA/winter_session_1/SWEA2383/SWEA2383.java
@@ -0,0 +1,23 @@
+package study.SWEA.winter_session_1.SWEA2383;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 점심 식사시간
+ * 링크 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5-BEE6AK0DFAVl&categoryId=AV5-BEE6AK0DFAVl&categoryType=CODE&problemTitle=%EC%A0%90%EC%8B%AC&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+ */
+
+public class SWEA2383 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+    }
+}
+
+/*
+
+
+*/
