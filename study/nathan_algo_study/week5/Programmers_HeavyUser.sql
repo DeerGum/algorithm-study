f0058984a17173a2eead08de4dd7247178daa740
황정준 <hsk2454@naver.com>
2022-04-02 00:36:46 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week5/Programmers_HeavyUser.sql
+++ study/nathan_algo_study/week5/Programmers_HeavyUser.sql
@@ -3,6 +3,14 @@
  * 링크 : https://programmers.co.kr/learn/courses/30/lessons/77486
  */
 
-
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(HOST_ID) >= 2
+)
+ORDER BY ID;
 
 
