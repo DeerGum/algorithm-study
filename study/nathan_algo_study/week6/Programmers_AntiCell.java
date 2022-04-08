5b19534ce195a2173ff42009bf7b889b8a01d392
황정준 <hsk2454@naver.com>
2022-04-09 00:57:01 +0900

    문제풀이 업로드

---
+++ study/nathan_algo_study/week6/Programmers_AntiCell.java
@@ -0,0 +1,14 @@
+package study.nathan_algo_study.week6;
+
+/**
+ * 문제이름 : 안티세포
+ * 링크 : https://programmers.co.kr/learn/courses/30/lessons/86054
+ */
+
+public class Programmers_AntiCell {
+
+}
+
+/*
+
+ */
---
+++ study/nathan_algo_study/week7/Baekjoon2075.java
@@ -0,0 +1,24 @@
+package study.nathan_algo_study.week7;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : N번째 큰 수
+ * 링크 : https://www.acmicpc.net/problem/2075
+ */
+
+public class Baekjoon2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+
+    }
+}
+
+/*
+
+*/
