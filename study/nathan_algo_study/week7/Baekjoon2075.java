ec379a009f7950f298d0e1b158dec552634fe57a
황정준 <hsk2454@naver.com>
2022-04-11 16:09:09 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week7/Baekjoon2075.java
+++ study/nathan_algo_study/week7/Baekjoon2075.java
@@ -9,11 +9,17 @@ import java.util.StringTokenizer;
  */
 
 public class Baekjoon2075 {
+    static int[][] map;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        
 
 
     }
