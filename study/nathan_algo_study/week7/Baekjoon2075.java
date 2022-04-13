411e149b847aee4c1f91cc45514fc1e6d0445cc4
황정준 <hsk2454@naver.com>
2022-04-13 23:35:34 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week7/Baekjoon2075.java
+++ study/nathan_algo_study/week7/Baekjoon2075.java
@@ -1,6 +1,8 @@
 package study.nathan_algo_study.week7;
 
 import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
@@ -9,22 +11,32 @@ import java.util.StringTokenizer;
  */
 
 public class Baekjoon2075 {
-    static int[][] map;
     static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
 
-        
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < N-1; i++)
+            pq.poll();
+
+        System.out.println(pq.poll());
 
 
     }
 }
 
 /*
+
 
 */
