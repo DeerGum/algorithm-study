9b3dadad13d5288b6989beaf9c384c537cfe7fd5
Jeongjun <hsk2454@naver.com>
2022-06-04 18:23:47 +0900

    임시 저장

---
+++ study/nathan_algo_study/week15/Baekjoon14426.java
@@ -0,0 +1,34 @@
+package study.nathan_algo_study.week15;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 접두사 찾기
+ * 링크 : https://www.acmicpc.net/problem/14426
+ */
+
+public class Baekjoon14426 {
+    static int N, M;
+    static String[] S;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+    public static boolean check(String str) {
+
+        return false;
+    }
+}
+
+/*
+
+*/
