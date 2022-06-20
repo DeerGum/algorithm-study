0bf76e338e64c484fe03ff1c514f3054b42255f4
Jeongjun <hsk2454@naver.com>
2022-06-20 17:39:38 +0900

    문제풀이 임시 저장

---
+++ study/nathan_algo_study/week17/Baekjoon1019.java
@@ -0,0 +1,40 @@
+package study.nathan_algo_study.week17;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 책 페이지
+ * 링크 : https://www.acmicpc.net/problem/1019
+ */
+
+public class Baekjoon1019 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+1 ~ 10
+1 2 3 4 5 6 7 8 9 10
+1 2 1 1 1 1 1 1 1 1
+
+1 ~ 9
+0 1 1 1 1 1 1 1 1 1
+
+11 ~ 20
+11 12 13 14 15 16 17 18 19 20
+
+1 10 2 1 1 1 1 1 1 1
+
+1 ~`19
+1 12 2 2 2 2 2 2 2 2
+
+
+
+
+*/
