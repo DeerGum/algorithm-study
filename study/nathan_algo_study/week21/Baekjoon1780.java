a75861b32437094d8ce7fc03285229a92c8dfefe
황정준 <hsk2454@naver.com>
2022-07-17 13:56:58 +0900

    임시저장

---
+++ study/nathan_algo_study/week21/Baekjoon1780.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week21;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 종이의 개수
+ * 링크 : https://www.acmicpc.net/problem/1780
+ */
+
+public class Baekjoon1780 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+    }
+}
+
+/*
+
+*/
