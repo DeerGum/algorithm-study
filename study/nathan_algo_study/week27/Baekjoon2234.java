f3e19c6353f5317df48b37ecaabd7bed73847d9f
Jeongjun <hsk2454@naver.com>
2022-08-28 15:56:58 +0900

    ì임시 저장

---
+++ study/nathan_algo_study/week27/Baekjoon2234.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week27;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 성곽
+ * 링크 : https://www.acmicpc.net/problem/2234
+ */
+
+public class Baekjoon2234 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
---
+++ study/nathan_algo_study/week27/Baekjoon3085.java
@@ -0,0 +1,22 @@
+package study.nathan_algo_study.week27;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 사탕 게임
+ * 링크 : https://www.acmicpc.net/problem/3085
+ */
+
+public class Baekjoon3085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        
+    }
+}
+
+/*
+
+*/
