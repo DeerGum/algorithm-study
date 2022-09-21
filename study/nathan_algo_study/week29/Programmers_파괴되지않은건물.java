b295595c641fecce1ebb6976869c8b4e95ef0ab6
황정준 <hsk2454@naver.com>
2022-09-21 22:02:16 +0900

    문제풀이 업로드

---
+++ study/nathan_algo_study/week29/Programmers_파괴되지않은건물.java
@@ -0,0 +1,12 @@
+package study.nathan_algo_study.week29;
+
+public class Programmers_파괴되지않은건물 {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+
+        return answer;
+    }
+}
+/*
+
+ */
