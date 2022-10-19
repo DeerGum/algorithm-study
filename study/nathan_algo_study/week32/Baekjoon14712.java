efdebface6ad369571c0b925ebfa734f56cb716f
황정준 <hsk2454@naver.com>
2022-10-19 23:19:24 +0900

    임시저장

---
+++ study/nathan_algo_study/week32/Baekjoon14712.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week32;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 넴모넴모 (Easy)
+ * 링크 : https://www.acmicpc.net/problem/14712
+ */
+
+public class Baekjoon14712 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
