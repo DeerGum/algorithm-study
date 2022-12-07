2ec9812ba8da9a446c76decf3f804c9ccaf9b187
황정준 <hsk2454@naver.com>
2022-12-07 22:57:54 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week39/Baekjoon1339.java
+++ study/nathan_algo_study/week39/Baekjoon1339.java
@@ -10,7 +10,6 @@ import java.util.*;
 
 public class Baekjoon1339 {
     static Character[] alphabets;
-    static Map<Character, Integer> map;
     static int N;
     static int maxValue;
     static String[] words;
@@ -29,41 +28,50 @@ public class Baekjoon1339 {
                 set.add(input.charAt(j));
         }
 
+        maxValue = 0;
         alphabets = set.toArray(new Character[0]);
+        perm(0, alphabets.length, new int[alphabets.length], new boolean[10]);
 
-        perm(0, words.length, new int[words.length], new boolean[words.length]);
-
-
+        System.out.println(maxValue);
     }
 
     public static void perm(int cnt, int n, int[] p, boolean[] v) {
         if (cnt == n) {
-            map = new HashMap<>();
+            Map<Character, Integer> map = new HashMap<>();
             for (int i = 0; i < n; i++) {
                 map.put(alphabets[i],p[i]);
             }
 
+            findMaxSum(map);
             return;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= 9; i++) {
             if (v[i])
                 continue;
 
             p[cnt] = i;
+            v[i] = true;
+            perm(cnt+1, n, p, v);
+            v[i] = false;
         }
-
     }
 
-    public static void findMaxSum() {
+    public static void findMaxSum(Map<Character, Integer> map) {
+        StringBuilder num;
+        int result = 0;
+        for (String s : words) {
+            num = new StringBuilder();
+            for (int i = 0; i < s.length(); i++)
+                num.append(map.get(s.charAt(i)));
 
+            result += Integer.valueOf(num.toString());
+        }
 
+        maxValue = Math.max(maxValue, result);
     }
 }
 
 /*
-
-1000000000
-
 
 */
