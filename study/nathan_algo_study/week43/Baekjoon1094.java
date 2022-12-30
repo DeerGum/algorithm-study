2e7364dd171b00a3bd24bc552c9ecce84d850b82
황정준 <hsk2454@naver.com>
2022-12-30 20:28:19 +0900

    임시저장

---
+++ study/nathan_algo_study/week43/Baekjoon1094.java
@@ -0,0 +1,32 @@
+package study.nathan_algo_study.week43;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 
+ * 링크 : https://www.acmicpc.net/problem/
+ */
+
+public class Baekjoon1094 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int ruler = 64;
+
+        while (ruler != n) {
+
+        }
+    }
+}
+
+/*
+64
+32
+16 16
+
+
+*/
