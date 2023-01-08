38289ab412de1b04876ab7446d77bd1f1dd08c89
황정준 <hsk2454@naver.com>
2023-01-08 14:10:40 +0900

    임시저장

---
+++ study/nathan_algo_study/week44/Baekjoon26257.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week44;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : std::shared_ptr
+ * 링크 : https://www.acmicpc.net/problem/26257
+ */
+
+public class Baekjoon26257 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        
+    }
+}
+
+/*
+
+*/
