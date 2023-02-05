202929e1d2a4af7608ed13f103804340891dfcd4
Jeongjun <hsk2454@naver.com>
2023-02-05 17:29:15 +0900

    임시저장

---
+++ study/nathan_algo_study/week46/Programmers_징검다리.java
@@ -0,0 +1,19 @@
+package study.nathan_algo_study.week46;
+
+/**
+ * 문제이름 : 징검다리
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/43236
+ */
+
+public class Programmers_징검다리 {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        
+
+        return answer;
+    }
+}
+
+/*
+
+*/
---
+++ study/nathan_algo_study/week46/Programmers_최소직사각형.java
@@ -0,0 +1,30 @@
+package study.nathan_algo_study.week46;
+
+/**
+ * 문제이름 : 최소직사각형
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/86491
+ */
+
+public class Programmers_최소직사각형 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
+
+/*
+80 70
+
+60 50 - 60 50
+30 70 - 60 70
+60 30 - 60 70
+80 40 - 80 70
+
+60 50 - 60 50
+70 30 - 70 50
+60 30 - 60 50
+80 40 - 80 50
+
+*/
