f67774e7d6e4b210d305369d4f39682a4c24978e
Jeongjun <hsk2454@naver.com>
2023-03-26 21:44:56 +0900

    문제풀이 임시저장

---
+++ study/nathan_algo_study/week50/Programmers_당구연습.java
@@ -0,0 +1,35 @@
+package study.nathan_algo_study.week50;
+
+/**
+ * 문제이름 : 당구 연습
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/169198
+ */
+
+public class Programmers_당구연습 {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
+
+/*
+3 7
+5 10
+7 7
+
+3 7
+7 13
+
+4 6
+
+16 36
+
+52
+
+2 13
+3 7
+
+
+
+*/
