7043032b2b0c13cb5125857c9d9acb2895c2b807
황정준 <hsk2454@naver.com>
2023-04-01 15:13:16 +0900

    임시저장

---
+++ study/nathan_algo_study/week51/Baekjoon20291.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week51;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 파일 정리
+ * 링크 : https://www.acmicpc.net/problem/20291
+ */
+
+public class Baekjoon20291 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
