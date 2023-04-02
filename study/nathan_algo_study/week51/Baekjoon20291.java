25b0ab34acba286e3271994fac724e822740ca23
Jeongjun <hsk2454@naver.com>
2023-04-02 21:47:34 +0900

    임시저장

--- study/nathan_algo_study/week51/Baekjoon20291.java
+++ study/nathan_algo_study/week51/Baekjoon20291.java
@@ -1,7 +1,8 @@
 package study.nathan_algo_study.week51;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 문제이름 : 파일 정리
@@ -12,9 +13,19 @@ public class Baekjoon20291 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        Map<String, Integer> map = new TreeMap<>();
 
+        int n = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < n; i++) {
+            String ext = br.readLine().split("\\.")[1];
+            map.put(ext, map.getOrDefault(ext, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet())
+            bw.write(entry.getKey() + " " + entry.getValue() + "\n");
+
+        bw.close();
     }
 }
 
---
+++ study/nathan_algo_study/week51/Baekjoon2170.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week51;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 선긋기
+ * 링크 : https://www.acmicpc.net/problem/2170
+ */
+
+public class Baekjoon2170 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
