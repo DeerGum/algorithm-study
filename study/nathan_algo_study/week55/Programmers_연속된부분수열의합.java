44d927a45043a1c29b377fb31e1e1d724a312347
Jeongjun <hsk2454@naver.com>
2023-05-07 19:50:55 +0900

    임시저장

---
+++ study/nathan_algo_study/week55/Programmers_연속된부분수열의합.java
@@ -0,0 +1,18 @@
+package study.nathan_algo_study.week55;
+
+/**
+ * 문제이름 : 연속된 부분 수열의 합
+ * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/178870
+ */
+
+public class Programmers_연속된부분수열의합 {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
+
+/*
+
+*/
