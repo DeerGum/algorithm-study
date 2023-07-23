ef4dd6d046b17dd2c7beca19cd831b4a0fb269e5
황정준 <hsk2454@naver.com>
2023-07-23 21:39:29 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week61/Programmers_조이스틱.java
+++ study/nathan_algo_study/week61/Programmers_조이스틱.java
@@ -3,35 +3,34 @@ package study.nathan_algo_study.week61;
 /**
  * 문제이름 : 조이스틱
  * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42860
+ * https://developingbear.tistory.com/169
  */
 
 public class Programmers_조이스틱 {
     public int solution(String name) {
         int[] cost = new int[name.length()];
-        for (int i = 0; i < cost.length; i++)
-            cost[i] = moveCharMinCnt('A', name.charAt(i));
-
         int answer = 0;
-        int idx = 0;
+
+        for (int i = 0; i < cost.length; i++)
+            answer += Math.min(name.charAt(i) - 'A', 'Z' + 1 - name.charAt(i)); //알파벳변경 코스트 계산
+
+        int moveCnt = Integer.MAX_VALUE;
         for (int i = 0; i < name.length(); i++) {
-            answer += moveCharMinCnt('A', name.charAt(i));
-            if (i != name.length() - 1)
-                answer++;   //옆으로 한칸 이동
+            int nextIdx = i + 1;   //현재 알파벳 이후 처음 만나는 A가 아닌 알파벳 인덱스
+            while (nextIdx < name.length() && name.charAt(nextIdx) == 'A')
+                nextIdx++;
+
+            //i에 있는 알파벳이 A이면서 name의 마지막 인덱스이면
+            if (name.charAt(i) == 'A' && nextIdx == name.length() + 1)
+                break;
+
+            //좌측, 우측으로 이동하는 포인터
+            int right = i * 2 + name.length() - nextIdx;
+            int left = i + (name.length() - nextIdx) * 2;
+            moveCnt = Math.min(moveCnt, Math.min(left, right));
         }
 
-        return answer;
-    }
-
-    private int moveCharMinCnt(char origin, char target) {
-        int cnt1 = Math.abs(origin - target + 26);
-        int cnt2 = Math.abs(target - origin);
-        System.out.println(String.format("%d , %d", cnt1, cnt2));
-        return Math.min(cnt1, cnt2);
-    }
-
-    private int calculateCost(int dir, int currIdx, int[] arr) {
-
-        return 0;
+        return answer + moveCnt;    //알파벳변경 코스트 + 이동 코스트
     }
 }
 
