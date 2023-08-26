97623c4556067df3ac980900b00d75b7f01aa39b
황정준 <hsk2454@naver.com>
2023-08-27 03:01:27 +0900

    임시저장

---
+++ study/nathan_algo_study/Goorm0000.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제이름 :
+ * 링크 : https://level.goorm.io/exam/
+ */
+
+public class Goorm0000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
---
+++ study/nathan_algo_study/week64/Goorm195688.java
@@ -0,0 +1,23 @@
+package study.nathan_algo_study.week64;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 문제이름 : 구름 찾기 깃발
+ * 링크 : https://level.goorm.io/exam/195689/%EA%B5%AC%EB%A6%84-%EC%B0%BE%EA%B8%B0-%EA%B9%83%EB%B0%9C/quiz/1
+ */
+
+public class Goorm195688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
+
+/*
+
+*/
