d57617d1854a8ebc3eb8d3090e52d0f5f31a7b3b
Jeongjun <hsk2454@naver.com>
2023-08-27 17:32:29 +0900

    문제풀이 업로드

--- study/nathan_algo_study/week64/Goorm195688.java
+++ study/nathan_algo_study/week64/Goorm195688.java
@@ -9,15 +9,55 @@ import java.util.StringTokenizer;
  */
 
 public class Goorm195688 {
+    static int[][] dir = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    static int[][] board;
+    static int N, K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++)
+                board[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1)
+                    continue;
+
+                if (findGoormCnt(i, j) == K)
+                    cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static int findGoormCnt(int r, int c) {
+        int cnt = 0;
+        for (int i = 0; i < dir.length; i++) {
+            int dr = r + dir[i][0];
+            int dc = c + dir[i][1];
+
+            if (dr < 0 || dr >= N || dc < 0 || dc >= N)
+                continue;
+
+            if (board[dr][dc] == 1)
+                cnt++;
+        }
+
+        return cnt;
     }
 }
 
 /*
 
-*/
+ */
